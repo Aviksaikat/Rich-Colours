148d8a9f96b12beb5a9f88b036d0b4bd8fe5b6b0
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2024-05-30 22:07:27 +0530

    Updated and modified files for the project - Added new files:   - demo.tape   - media/demo.gif - Updated files:   - requirements-dev.lock   - requirements.lock   - src/rich_colours/generate.py

--- src/rich_colours/generate.py
+++ src/rich_colours/generate.py
@@ -1,7 +1,12 @@
 from rich.color import ANSI_COLOR_NAMES
 from rich.console import Console
-
+from time import sleep
 console = Console()
 
+i = 0
 for colour in ANSI_COLOR_NAMES.keys():
+    if i % 10 == 0:
+        ...# take a screenshot
     console.print(f"[{colour}]The accent for this line is : {colour}")
+    sleep(0.2)
+    i += 1
